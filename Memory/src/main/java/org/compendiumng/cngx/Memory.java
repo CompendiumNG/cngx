10e3bc6811c9e98d95d32535093f0e6587feffe3
Michal Stekrt <michal@is76.org>
2014-02-22 23:21:25 +0100

    WIP (work in progress) - add main modules with dummy code

---
+++ Memory/src/main/java/org/compendiumng/cngx/Memory.java
@@ -0,0 +1,10 @@
+package  org.compendiumng.cngx;
+
+import java.lang.String;
+
+class Memory  {
+    public static void main (String[] args) {
+        System.out.println("haha I am the Memory");
+    }
+
+}
